f4a4336fafbee3af9e21ed388c8917d460c94cf7
René Schultz Madsen <rm@microting.com>
2022-12-16 19:27:55 +0000

    Fixing build script to lates versions.

---
+++ delete.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+OWNER="microting"
+REPO="work-items-planning-container"
+
+workflow_ids=($(gh api repos/$OWNER/$REPO/actions/workflows | jq '.workflows[] | .id'))
+
+for workflow_id in "${workflow_ids[@]}"
+do
+ echo "Listing runs for the workflow ID $workflow_id"
+ run_ids=( $(gh api repos/$OWNER/$REPO/actions/workflows/$workflow_id/runs --paginate | jq '.workflow_runs[].id') )
+ for run_id in "${run_ids[@]}"
+ do
+   echo "Deleting Run ID $run_id"
+   gh api repos/$OWNER/$REPO/actions/runs/$run_id -X DELETE >/dev/null
+ done
+done
